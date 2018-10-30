264095321d801b8bae7dbadf8ab63f04eedfdeb7
MC Simoes <mariacristina.simoes@gmail.com>
2018-10-30 20:20:54 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,12 +4,6 @@
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
 def valid_move?(board, index)
-  if board[index].between(1,9)
-    return true
-  else 
-    return false
-  end
-  
 def position_taken?(board, index)
 taken = nil
 if (board[index] == " " || board[index] == "" || board[index] == nil)
